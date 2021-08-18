ee817c7d3f46cec17d3fffbab3391e48c705b194
Karanjot Singh <jo@jOs-MacBook.local>
2021-08-18 13:48:15 +0530

    manifest v3; removed tabs permission; minor UI changes

--- content_security.js
+++ content_security.js
@@ -2,7 +2,7 @@ $(window).on("load", () => {
     chrome.storage.sync.get(['q1', 'a1', 'q2', 'a2', 'q3', 'a3', 'q4', 'a4'], function (items) {
         setTimeout(() => {
             if ($('strong')[0].innerText == '\"'+items['q1']+'\"') {
-                $('#answer').val('a1');
+                $('#answer').val(items['a1']);
             } else if ($('strong')[0].innerText == '\"'+items['q2']+'\"') {
                 $('#answer').val(items['a2']);
             } else if ($('strong')[0].innerText == '\"'+items['q3']+'\"') {
